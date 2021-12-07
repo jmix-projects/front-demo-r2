6af88070aec87362776d2ed5c912e6b8dea08642
Nadezhda Shatalova <n.shatalova@haulmont.com>
2021-12-07 12:35:30 +0400

    add GraphQlRestoreRole and liquibase script for it

---
+++ src/main/java/io/jmix/frontdemo/security/GraphQlRestoreRole.java
@@ -0,0 +1,11 @@
+package io.jmix.frontdemo.security;
+
+import io.jmix.security.role.annotation.GraphQLPolicy;
+import io.jmix.security.role.annotation.ResourceRole;
+
+@ResourceRole(name = "GraphQlRole", code = "graph-ql-restore-role")
+public interface GraphQlRestoreRole {
+
+    @GraphQLPolicy(operations = "restoreEntities")
+    void graphqlPolicy();
+}
