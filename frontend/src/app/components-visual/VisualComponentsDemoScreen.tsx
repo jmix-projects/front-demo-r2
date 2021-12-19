d6ed0ef53803910817bcf6be34c26d90ed2705ca
Vlad Minaev <minaev@haulmont.com>
2021-12-19 23:57:10 +0400

    Add screen with visual components #7

--- frontend/src/app/components-visual/VisualComponentsDemoScreen.tsx
+++ frontend/src/app/components-visual/VisualComponentsDemoScreen.tsx
@@ -1,21 +1,92 @@
 import React from "react";
-import {EntityProperty, registerScreen} from "@haulmont/jmix-react-web";
+import {EntityProperty, Label, registerScreen} from "@haulmont/jmix-react-web";
 import {Card, Form, Space, Typography} from "antd";
 import Link from "antd/lib/typography/Link";
 import Paragraph from "antd/es/typography/Paragraph";
 import {useForm} from "antd/es/form/Form";
-import {DataTable, Field} from "@haulmont/jmix-react-antd";
+import {
+  Card as JmixCard,
+  Col,
+  DataTable,
+  EntityHierarchyTree,
+  Field,
+  Footer,
+  Header,
+  MultiTabs,
+  Row,
+  Sidebar,
+} from "@haulmont/jmix-react-antd";
+import styles from './style.module.css'
 
 const FIELD_CODE = `<Field/>`;
 const FORM_CODE = `<Form/>`;
 const DATA_TABLE_CODE = `<DataTable/>`;
 const ENTITY_PROPERTY_CODE = `<EntityProperty/>`;
 
-
 export const VisualComponentsDemoScreen = () => {
   const [form] = useForm();
+
   return (
     <Space direction={"vertical"} style={{width: "100%"}}>
+      <Card title={"Grid"}>
+        <Row>
+          <Col span={8} className={styles.colBg}>Col-8</Col>
+          <Col span={8} className={styles.colBg}>Col-8</Col>
+          <Col span={8} className={styles.colBg}>Col-8</Col>
+        </Row>
+        <Row>
+          <Col span={3}>Col-3</Col>
+          <Col span={18}>Col-18</Col>
+          <Col span={3}>Col-3</Col>
+        </Row>
+      </Card>
+      <Card title="Header">
+        <Header>
+          <Typography.Title level={3} style={{color: '#FFF', padding: '15px 0'}}>
+            Header content
+          </Typography.Title>
+        </Header>
+      </Card>
+      <Card title="Footer">
+        <Footer>Footer content</Footer>
+      </Card>
+      <Card title="Sidebar">
+        <Sidebar style={{minHeight: "100px"}}>
+        </Sidebar>
+      </Card>
+      <Card title="MultiTabs">
+        <MultiTabs/> { /* todo extract for demonstration */}
+      </Card>
+      <Card title="Card">
+        <JmixCard title={"Card Title"}>
+          <p>Card content</p>
+        </JmixCard>
+      </Card>
+      <Card title="Label">
+        <Label entityName="Order_" propertyName="numberOfSpecialProducts">
+        </Label>
+      </Card>
+      <Card>
+        <EntityHierarchyTree
+          items={[
+            {
+              id: '1',
+              _instanceName: 'Instance A',
+              parent: undefined,
+            },
+            {
+              id: '2',
+              _instanceName: 'Instance B',
+              parent: '1'
+            },
+            {
+              id: ' 3',
+              _instanceName: 'Instance C',
+              parent: '2'
+            }]}
+          hierarchyProperty='parent'
+        />
+      </Card>
       <Card title={"Field"}>
         <Paragraph>
           <Typography.Text code>{FIELD_CODE}</Typography.Text> - Renders UI component for given entity attribute,
