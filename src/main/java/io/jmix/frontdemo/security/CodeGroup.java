fedb22585779d247e12ced179a4ff79066176947
subbotin <a.subbotin@haulmont.com>
2021-12-28 16:55:59 +0400

    Add jmix test roles and groups

---
+++ src/main/java/io/jmix/frontdemo/security/CodeGroup.java
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright 2020 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.frontdemo.security;
+
+import io.jmix.frontdemo.entity.User;
+import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
+import io.jmix.security.role.annotation.RowLevelRole;
+
+@RowLevelRole(name = "CodeGroup", code = CodeGroup.CODE)
+public interface CodeGroup {
+    String CODE = "CodeGroup";
+
+    @JpqlRowLevelPolicy(entityClass = User.class, join = "join2", where = "where2")
+    void userRowLevelPolicies();
+}
